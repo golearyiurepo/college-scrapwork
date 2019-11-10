19c4462ab7702fc41cd99337e62f0902325dfca7
Gavin O'Leary <goleary@149-160-147-85.dhcp-bl.indiana.edu>
2019-11-10 11:36:37 -0500

    changes

--- ttt.py
+++ ttt.py
@@ -7,15 +7,15 @@ def drawBoard(board):
 
      # "board" is a list of 10 strings representing the board (ignore index 0)
      print('   |   |')
-     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+     print(' ' + board[7] + '  | ' + board[8] + '  | ' + board[9])
      print('   |   |')
      print('-----------')
      print('   |   |')
-     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+     print(' ' + board[4] + '  | ' + board[5] + '  | ' + board[6])
      print('   |   |')
      print('-----------')
      print('   |   |')
-     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+     print(' ' + board[1] + '  | ' + board[2] + '  | ' + board[3])
      print('   |   |')
 
 def playPLetter():
