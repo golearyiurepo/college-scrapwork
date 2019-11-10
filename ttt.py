1c8d91b35b4c5f5536c42b31b8514c0457c0b24b
Gavin O'Leary <goleary@149-160-147-85.dhcp-bl.indiana.edu>
2019-11-10 18:59:28 -0500

    small comment

--- ttt.py
+++ ttt.py
@@ -6,6 +6,7 @@ def drawBoard(board):
      # This function prints out the board that it was passed.
 
      # "board" is a list of 10 strings representing the board (ignore index 0)
+     # We ignore index 0 to be consistent thru out program and help users
      print('   |   |')
      print(' ' + board[7] + '  | ' + board[8] + '  | ' + board[9])
      print('   |   |')
